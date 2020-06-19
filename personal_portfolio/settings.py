69335c7254804746c745b523429611be380739cb
shahsanjay10 <shahsanjaygla@gmail.com>
2020-06-19 11:45:21 +0000

    Remove cached

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k*c)oa#z$^8qm-b4bo$_bvesxjc=rs6$u5famr$y(9xl31nn+!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shahsanjay10.pythonanywhere.com']
 
 
 # Application definition
